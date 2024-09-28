6be2121ab6417d150e5ed81f240c99a7ff88735a
makatalin <makatalin@users.noreply.github.com>
2024-09-28 19:34:23 +0200

    Baza 28.9.

--- web-app/config/db.js
+++ web-app/config/db.js
@@ -1,17 +1,19 @@
-const { Database } = require('turso');
-require('dotenv').config();
+const sqlite3 = require('sqlite3').verbose();
+const path = require('path');
 
-const db = new Database(process.env.TURSO_URL, {
-  authToken: process.env.TURSO_AUTH_TOKEN
-});
+const dbPath = path.resolve(__dirname, '../PametniOtpad.db');
 
-db.query("PRAGMA foreign_keys = ON")
-  .then(() => {
-    console.log('Connected to Turso database');
-  })
-  .catch(err => {
+const db = new sqlite3.Database(dbPath, (err) => {
+  if (err) {
     console.error('Could not connect to database', err);
-  });
+  } else {
+    db.run("PRAGMA foreign_keys = ON", (err) => {
+      if (err) {
+        console.error('Error enabling foreign keys:', err);
+      }
+    });
+  }
+});
 
 module.exports = db;
 
