91d6df8d4f150dfb499b32e94eb754912b9a6a0a
Mario Katalinic <143451659+makatalin@users.noreply.github.com>
2024-09-28 13:23:14 +0200

    Update db.js

--- web-app/config/db.js
+++ web-app/config/db.js
@@ -1,14 +1,17 @@
-const path = require('path');
-const sqlite3 = require('sqlite3').verbose();
+const { Database } = require('turso');
+require('dotenv').config();
 
-const dbPath = path.join(__dirname, 'PametniOtpad.db');
-const db = new sqlite3.Database(dbPath, (err) => {
-    if (err) {
-        console.error('Could not connect to database', err);
-    } else {
-        db.run("PRAGMA foreign_keys = ON");
-    }
+const db = new Database(process.env.TURSO_URL, {
+  authToken: process.env.TURSO_AUTH_TOKEN
 });
+
+db.query("PRAGMA foreign_keys = ON")
+  .then(() => {
+    console.log('Connected to Turso database');
+  })
+  .catch(err => {
+    console.error('Could not connect to database', err);
+  });
 
 module.exports = db;
 
