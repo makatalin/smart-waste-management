57d4222dde2193ba963578b3dd8be3bab432532f
Mario Katalinic <143451659+makatalin@users.noreply.github.com>
2025-11-30 19:43:45 +0100

    Fix closing div tag in korisnici.js

--- web-app/public_files/korisnici.js
+++ web-app/public_files/korisnici.js
@@ -239,7 +239,7 @@ function displayUsers(usersToDisplay) {
             <div class="korisnik-stupac col-telefon">${user.telefon}</div>
             <div class="korisnik-stupac col-adresa" data-full-address="${user.ulica} ${user.broj}">
                 <span class="address-text">${user.ulica} ${user.broj}</span>
-                </div>
+            </div>
             <div class="korisnik-stupac col-grad">${user.grad}</div>
             <div class="korisnik-stupac col-registracija">${formatDateHR(user.datum_registracije)}</div>
             <div class="korisnik-stupac col-odjava">${user.datum_odjave ? formatDateHR(user.datum_odjave) : ''}</div>
